c9579929af4998b880d8100914b98cfae1f48b3b
Sébastien Lorrain <sldeveloperand@gmail.com>
2020-05-25 13:54:54 +0200

    sensorwidget call API and update

--- app/src/main/java/fr/geringan/activdash/appwidgets/SensorWidget.java
+++ app/src/main/java/fr/geringan/activdash/appwidgets/SensorWidget.java
@@ -1,59 +1,73 @@
 package fr.geringan.activdash.appwidgets;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.widget.RemoteViews;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import fr.geringan.activdash.R;
+import fr.geringan.activdash.helpers.PrefsManager;
+import fr.geringan.activdash.models.SensorDataModel;
 import fr.geringan.activdash.network.GetHttp;
-import fr.geringan.activdash.providers.IconProvider;
 
 public class SensorWidget extends AppWidgetProvider {
     private final static String ACTION_SENSOR = "fr.geringan.activdashwidget.action.REFRESH_SENSOR";
     public static String ACTIONURL_EXTRA = "actionUrl";
+    public static CustomRemoteViews remoteViews;
 
-    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                       int appWidgetId) {
+    public static void updateAppWidget(
+            Context context,
+            AppWidgetManager appWidgetManager,
+            int appWidgetId
+    ) {
+        remoteViews = new CustomRemoteViews(context.getPackageName(), R.layout.sensor_widget);
+        PrefsManager.launch(context);
 
         CharSequence widgetText = SensorWidgetConfigureActivity.loadPrefs(context, appWidgetId).get(0);
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.sensor_widget);
-        remoteViews.setTextViewText(R.id.appwidget_text, widgetText);
+        remoteViews.setTextViewText(R.id.sensor_widget_configure_text, widgetText);
 
-        CharSequence widgetHttp = DashWidgetConfigureActivity.loadPrefs(context, appWidgetId).get(1);
-        Intent intent = new Intent(context, SensorWidget.class);
-        intent.setAction(ACTION_SENSOR);
-        intent.putExtra(ACTIONURL_EXTRA, widgetHttp);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.sensor_widget_layout, pendingIntent);
+        CharSequence widgetHttp = SensorWidgetConfigureActivity.loadPrefs(context, appWidgetId).get(1);
+
+        //Call the REST APIs
+        String thermostatUrl = String.valueOf(widgetHttp);
+        callSensorApi(appWidgetManager, appWidgetId, thermostatUrl);
 
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+    }
+
+    public static void callSensorApi(AppWidgetManager appWidgetManager, int appWidgetId, String sensorUrl) {
+        GetHttp getData = new GetHttp();
+        getData.setOnResponseListener(response -> {
+            try {
+                JSONArray jsonArray = new JSONArray(response);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    SensorDataModel sensor = new SensorDataModel(obj);
+                    String temperature = sensor.getValeur1();
+                    String name = sensor.getNom();
+                    if (null != remoteViews) {
+                        remoteViews.setTextViewText(R.id.sensor_widget_value, temperature);
+                        remoteViews.setTextViewText(R.id.sensor_widget_name, name);
+                        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+        getData.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, sensorUrl);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        if (ACTION_SENSOR.equals(intent.getAction())) {
-            String url = intent.getStringExtra(ACTIONURL_EXTRA);
-            GetHttp getData = new GetHttp();
-            getData.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, url);
-        }
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
-            SensorWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
         }
     }
 }
